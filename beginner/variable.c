f177efe450eee4406a995bcd95a976238130473f
umeshbalaji <34675971+umeshbalaji@users.noreply.github.com>
2017-12-19 14:21:41 +0530

    Create variable.c

---
+++ beginner/variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char name[30]="Sachin Ramesh Tendulkar";
+char nickname[30]="Little Master";
+int run=18426;
+float average=44.83;
+int matches=463;
+printf("%s\n",name);
+printf("%s\n",nickname);
+printf("%d\n",run);
+printf("%.2f\n",average);
+printf("%d\n",matches);
+}
